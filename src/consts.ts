93dfda82028ef706b74fd1c130436698751847aa
emosbaugh <emosbaugh@gmail.com>
2017-10-26 16:15:06 -0700

    Run and deploy (#7)
    * Run and deploy
    
    * Circle publish
    
    * npm test
    
    * yarn

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 
 export default {
-  localPath: "/replicated",
+  localPath: "./replicated",
   // upstreamEndpoint: "http://192.168.100.100:8006/market",
   upstreamEndpoint: "https://api.replicated.com/market/",
 };
