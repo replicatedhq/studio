e3d370bd4ee04c6d00fec1dfeee8299d2ecd88de
emosbaugh <emosbaugh@gmail.com>
2017-11-21 15:39:43 -0800

    Don't enforce linting errors when listing releases (#18)
    * Don't enforce linting errors when listing releases [ch3779]
    
    * allow other truthy values

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,5 @@ export default {
   localPath: process.env.STUDIO_YAML_DIR || "./replicated",
   upstreamEndpoint: process.env.STUDIO_UPSTREAM_BASE_URL || "https://api.replicated.com/market/",
   lintThreshold: process.env.STUDIO_LINT_THRESHOLD || "error",
+  lintRequired: /^(?:y|yes|true|1)$/i.test(process.env.STUDIO_LINT_REQUIRED || ""),
 };
